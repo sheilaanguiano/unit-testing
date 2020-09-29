457cffefadaea3d8a233546d151929620da0820c
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-09-29 10:32:52 -0700

    basic capitalization test

--- textUtilities.js
+++ textUtilities.js
@@ -1,8 +1,15 @@
 const expect = require('chai').expect;
 
 function titleCase(title){
-    return title;
+    let words = title.split(' ');
+    const titleCasedWords = words.map(function (word) {
+        return word[0].toUpperCase() + word.substring(1);
+    });
+    return titleCasedWords.join(' ');
 
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
